330c5c15280f65509e98eadbe977ecf10ba897eb
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-06-26 20:55:34 -0500

    Created html, css, and js files. Added buttons. Started on database.

---
+++ main.js
@@ -0,0 +1,27 @@
+const restaurants = [];
+const database = [
+    {
+        type: 'asian',
+        name: 'Nama Sushi',
+        price: 3,
+        rating: 8
+    },
+    {
+        type: 'healthy',
+        name: 'Panera',
+        price: 2,
+        rating: 8
+    },
+    {
+        type: 'mexican',
+        name: 'Tequila\'s',
+        price: 2,
+        rating: 8
+    }
+];
+
+function loadAll() {
+// add click listener to all button
+// loop over database and at each index, create a div, give the div text content of database[i].name
+// select the body and append the child
+}
